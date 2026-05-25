020e12c93500ae3ac95b95fd222c8cf4bc070613
Baconboi696 <baconboii696@gmail.com>
2026-05-25 22:10:16 +0530

    changes made

--- landscape-client/vite.config.js
+++ landscape-client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://landscape-client.onrender.com',
         changeOrigin: true,
       }
     }
